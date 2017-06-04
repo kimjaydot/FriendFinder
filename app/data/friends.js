178b03a2eb9d70b9129090a654cf6414523cc3ec
Kimberley Johnson <kimberleyjohnson@Kimberleys-MBP.attlocal.net>
2017-06-04 12:06:37 -0500

    add objects

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,10 +7,25 @@
 
 var friendArray = [
   {
-    customerName: "Saima",
-    customerEmail: "saima@example.com",
-    phoneNumber: "000-000-0000",
-    customerID: "saimacool"
+
+
+    aboutYouName: "Saima",
+    photoImage: "saima@example.com",
+    question1: "000-000-0000",
+    question2: "saimacool",
+    question3: "saimacool",
+    question4: "saimacool",
+    question5: "saimacool",
+    question6: "saimacool",
+    question7: "saimacool",
+    question8: "saimacool",
+    question9: "saimacool",
+    question10: "saimacool",
+    
+    // customerName: "Saima",
+    // customerEmail: "saima@example.com",
+    // phoneNumber: "000-000-0000",
+    // customerID: "saimacool"
   }
 ];
 
