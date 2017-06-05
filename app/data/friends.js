45456c7f25bda3b8777646ca3414bacdfd83e5a2
Kimberley Johnson <kimberleyjohnson@Kimberleys-MBP.attlocal.net>
2017-06-04 20:48:38 -0500

    add first friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -8,10 +8,25 @@
 var friendArray = [
   {
 
+// name: "kim",
+// photo: "kim@example.com",
+// scores: "numbers"
 
-name: "kim",
-photo: "kim@example.com",
-scores: "numbers"
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+
     
     // customerName: "Saima",
     // customerEmail: "saima@example.com",
